9e642ab82034916bd7a8fc572a15d859a38e8849
Joaquin Lucero <joaquinlucero194@gmail.com>
2024-10-23 21:00:09 -0300

    avances y acomodo un poco

---
+++ main.js
@@ -0,0 +1,9 @@
+function validar(event){
+    event.preventDefault()
+    let nombre = document.getElementById("nombre").value;
+    let edad = document.getElementById("edad").value;
+    let email = document.getElementById("email").value;
+    let errores = document.getElementById("errores").value;
+    let valido = true;
+    
+}
